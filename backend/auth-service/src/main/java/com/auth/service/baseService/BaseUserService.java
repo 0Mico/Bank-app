b219e3e7144e58a6cf14cef88bba0412bc95ca36
0Mico <michele.randazzo0100@gmail.com>
2026-04-02 16:21:15 +0200

    base service pattern inserted in auth-service

---
+++ backend/auth-service/src/main/java/com/auth/service/baseService/BaseUserService.java
@@ -0,0 +1,17 @@
+package com.auth.service.baseService;
+
+import com.auth.dto.ChangePasswordDto;
+import com.auth.entity.User;
+import com.auth.model.UserModel;
+import com.auth.repository.UserRepository;
+import com.common.interfaces.BaseService;
+
+public interface BaseUserService extends BaseService<UserRepository, User, Long> {
+    boolean checkIfUserExists(Long userId);
+    User getUserById(Long id);
+    User getUserByEmail(String email);
+    User getUserByIban(String iban);
+    User updateUser(Long id, UserModel userDTO);
+    void deleteUser(Long id);
+    void changePassword(Long id, ChangePasswordDto request);
+}
